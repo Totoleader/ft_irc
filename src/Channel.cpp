746480c032c5d0f05f3fb9d08d29a57e0a85f1ce
Maxil Cote <macote@c1r5p2.42quebec.com>
2023-12-05 11:15:09 -0500

    added channel functionnalities

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -4,6 +4,22 @@ Channel::Channel()
 {
 }
 
+Channel::Channel(std::string name, User &u): _name(name)
+{
+	addUser(u);
+	addModerator(u.getName());
+}
+
 Channel::~Channel()
 {
+}
+
+void Channel::addUser(User &user)
+{
+	_channelUsers[user.getName()] = user;
+}
+
+void Channel::addModerator(std::string modName)
+{
+	_moderatorName.push_back(modName);
 }
